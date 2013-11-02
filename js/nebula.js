51a53d16f486646135e007d1790e63b5f3a5577b
James Moriarty <jamespaulmoriarty@gmail.com>
2013-11-02 19:17:00 +1100

    update: js.

--- js/nebula.js
+++ js/nebula.js
@@ -11,11 +11,42 @@
   };
 
   Quintus.Math = function(Q) {
+    Q.normalizeAngle = function(angle) {
+      var result;
+      result = angle % 360;
+      if (result < 0) {
+        return result + 360;
+      } else {
+        return result;
+      }
+    };
+    Q.angle = function(fromX, fromY, toX, toY) {
+      var distX, distY, radians;
+      distX = toX - fromX;
+      distY = toY - fromY;
+      radians = Math.atan2(distY, distX);
+      return Q.normalizeAngle(Q.radiansToDegrees(radians));
+    };
+    Q.distance = function(fromX, fromY, toX, toY) {
+      if (toX == null) {
+        toX = 0;
+      }
+      if (toY == null) {
+        toY = 0;
+      }
+      return Math.sqrt(Math.pow(fromX - toX, 2), Math.pow(fromY - toY, 2));
+    };
     Q.offsetX = function(angle, radius) {
       return Math.sin(angle / 180 * Math.PI) * radius;
     };
-    return Q.offsetY = function(angle, radius) {
+    Q.offsetY = function(angle, radius) {
       return -Math.cos(angle / 180 * Math.PI) * radius;
+    };
+    Q.degreesToRadians = function(degrees) {
+      return degrees * (Math.PI / 180);
+    };
+    return Q.radiansToDegrees = function(radians) {
+      return radians * (180 / Math.PI);
     };
   };
 
@@ -30,7 +61,8 @@
 
   Q = Quintus().include('Util, Math, Sprites, Scenes, Input, 2D, Touch, UI, Audio').setup({
     development: true,
-    maximize: true
+    width: 640,
+    height: 480
   }).controls().touch().enableSound();
 
   Q.gravityY = 0;
@@ -39,7 +71,7 @@
 
   Q.clearColor = "#000";
 
-  Q.load(['ship.png', 'particle.png', 'background.png', 'star.png', 'menu.mp3', 'blasterShot.mp3'], function() {
+  Q.load(['ship.png', 'enemy.png', 'particle.png', 'background.png', 'star.png', 'menu.mp3', 'blasterShot.mp3'], function() {
     return Q.stageScene('Menu');
   }, {
     progressCallback: function(loaded, total) {
@@ -68,11 +100,15 @@
     stage.insert(new Q.Background({
       target: player
     }));
-    for (_i = 1, _ref = Q.width * Q.height / 5000; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+    for (_i = 1, _ref = Q.width * Q.height / 10000; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
       stage.insert(new Q.Star({
         target: player
       }));
     }
+    stage.insert(new Q.Enemy({
+      x: player.p.x,
+      y: player.p.y
+    }));
     stage.insert(player);
     stage.on('destroy', function() {
       return player.destroy;
@@ -123,26 +159,25 @@
     }));
   });
 
-  Q.Ship = Q.Sprite.extend('Ship', {
+  Q.Sprite.extend('Ship', {
     init: function(p) {
-      this._super(p, {
-        asset: 'ship.png',
-        z: 10
-      });
-      this.weapon = new Q.Blaster;
+      this._super(Q._extend({
+        type: Q.SPRITE_DEFAULT,
+        collisionMask: Q.SPRITE_ACTIVE
+      }, p));
       return this.add('2d');
     },
     fire: function() {
       return this.weapon.tryFire(this);
     },
     accelerate: function(dt) {
-      this.p.vx += Q.offsetX(this.p.angle, 100) * dt;
-      this.p.vy += Q.offsetY(this.p.angle, 100) * dt;
+      this.p.vx += Q.offsetX(this.p.angle, Q[this.className].acceleration) * dt;
+      this.p.vy += Q.offsetY(this.p.angle, Q[this.className].acceleration) * dt;
       return this.stage.insert(new Q.Particle({
         x: this.p.x - Q.offsetX(this.p.angle, this.p.cx),
         y: this.p.y - Q.offsetY(this.p.angle, this.p.cy),
-        vx: this.p.vx - Q.offsetX(this.p.angle, Math.max(this.p.vx * 0.1, 5)),
-        vy: this.p.vy - Q.offsetY(this.p.angle, Math.max(this.p.vy * 0.1, 5))
+        vx: this.p.vx - Q.offsetX(this.p.angle, Math.max(this.p.vx * 0.1, 75)),
+        vy: this.p.vy - Q.offsetY(this.p.angle, Math.max(this.p.vy * 0.1, 75))
       }));
     },
     turn: function(dt, degree) {
@@ -179,6 +214,7 @@
       });
     },
     drawOffset: 200,
+    debugRender: function() {},
     draw: function(ctx) {
       var offsetX, offsetY;
       if (this.stage.viewport) {
@@ -203,8 +239,8 @@
       accuracy = Math.floor((Math.random() * 5) - 5);
       angle = from.p.angle + accuracy;
       from.stage.insert(new Q.BlasterShot({
-        x: from.p.x + Q.offsetX(from.p.angle, from.p.cx * 2),
-        y: from.p.y + Q.offsetY(from.p.angle, from.p.cy * 2),
+        x: from.p.x + Q.offsetX(from.p.angle, from.p.cx * 2.5),
+        y: from.p.y + Q.offsetY(from.p.angle, from.p.cy * 2.5),
         vx: from.p.vx + Q.offsetX(angle, velocity),
         vy: from.p.vy + Q.offsetY(angle, velocity),
         angle: angle
@@ -218,12 +254,17 @@
 
   Q.Sprite.extend('BlasterShot', {
     init: function(p) {
-      this._super(p, {
+      this._super(Q._extend({
+        type: Q.SPRITE_ACTIVE,
+        collisionMask: Q.SPRITE_ACTIVE,
         asset: 'particle.png',
         z: 5,
         scale: 0.4
+      }, p));
+      this.add('2d');
+      return this.on('hit', function(col) {
+        return this.destroy();
       });
-      return this.add('2d');
     }
   });
 
@@ -232,7 +273,7 @@
       return this._super(p, {
         x: Math.random() * Q.width,
         y: Math.random() * Q.height,
-        scale: Math.random(),
+        scale: Math.max(Math.random(), .3),
         asset: 'star.png',
         type: Q.SPRITE_NONE
       });
@@ -248,23 +289,23 @@
 
   Q.Sprite.extend('Particle', {
     init: function(p) {
-      this._super(p, {
+      this._super(Q._extend({
         asset: 'particle.png',
         type: Q.SPRITE_NONE,
         z: 5,
         opacity: 0.5,
         scale: 0.5
-      });
-      this.add('2d');
-      return this.on('step', function(dt) {
-        this.p.vx *= 1 - dt;
-        this.p.vy *= 1 - dt;
-        if (this.p.scale >= 0) {
-          return this.p.scale -= dt / 2;
-        } else {
-          return this.destroy();
-        }
-      });
+      }, p));
+      return this.add('2d');
+    },
+    step: function(dt) {
+      this.p.vx *= 1 - dt;
+      this.p.vy *= 1 - dt;
+      if (this.p.scale >= 0) {
+        return this.p.scale -= dt;
+      } else {
+        return this.destroy();
+      }
     },
     draw: function(ctx) {
       ctx.globalCompositeOperation = 'lighter';
@@ -274,14 +315,20 @@
 
   Q.Ship.extend('Player', {
     init: function(p) {
-      this._super(p);
-      return Q.input.on('fire', this, 'fire');
+      this._super(Q._extend({
+        type: Q.SPRITE_DEFAULT,
+        asset: 'ship.png'
+      }, p));
+      return this.weapon = new Q.Blaster;
     },
     step: function(dt) {
       if (Q.inputs['up'] || Q.inputs['action']) {
         this.accelerate(dt);
       } else {
         this.friction(dt);
+      }
+      if (Q.inputs['fire']) {
+        this.fire();
       }
       if (Q.inputs['left']) {
         this.turn(dt, -100);
@@ -290,6 +337,8 @@
         return this.turn(dt, 100);
       }
     }
+  }, {
+    acceleration: 100
   });
 
   Q.Sprite.extend('Star', {
@@ -299,19 +348,53 @@
         x: Math.random() * Q.width,
         y: Math.random() * Q.height,
         asset: 'star.png',
-        scale: Math.random(),
+        scale: Math.max(Math.random(), .3),
         type: Q.SPRITE_NONE
       });
       return this.add('2d');
     },
     step: function(dt) {
-      this.p.vx = this.p.target.p.vx * (this.p.scale / -1);
-      this.p.vy = this.p.target.p.vy * (this.p.scale / -1);
+      this.p.vx = this.p.target.p.vx * (Math.pow(this.p.scale, 10) / -1);
+      this.p.vy = this.p.target.p.vy * (Math.pow(this.p.scale, 10) / -1);
       if (Math.abs(this.p.x - this.p.target.p.x) > Q.width || Math.abs(this.p.y - this.p.target.p.y) > Q.height) {
         this.p.x = Q.stage().viewport.x + (Math.random() * Q.width);
         return this.p.y = Q.stage().viewport.y + (Math.random() * Q.height);
       }
     }
+  });
+
+  Q.component('aiWander', {
+    added: function() {
+      return this.entity.on("step", this, "step");
+    },
+    step: function(dt) {
+      var targetAngle, velocityAngle;
+      if (!this.targetX || !this.targetY || 50 > Q.distance(this.entity.p.x, this.entity.p.y, this.targetX, this.targetY)) {
+        this.targetX = Math.random() * 1000;
+        this.targetY = Math.random() * 1000;
+      }
+      targetAngle = 90 + Q.angle(this.entity.p.x, this.entity.p.y, this.targetX, this.targetY);
+      if (this.entity.p.angle - targetAngle > 0) {
+        this.entity.turn(dt, -100);
+      } else {
+        this.entity.turn(dt, 100);
+      }
+      velocityAngle = 90 + Q.angle(this.entity.p.vx, this.entity.p.vy);
+      return this.entity.accelerate(dt);
+    }
+  });
+
+  Q.Ship.extend('Enemy', {
+    init: function(p) {
+      this._super(Q._extend({
+        type: Q.SPRITE_DEFAULT | Q.SPRITE_ENEMY,
+        collisionMask: Q.SPRITE_ACTIVE,
+        asset: 'enemy.png'
+      }, p));
+      return this.add('aiWander');
+    }
+  }, {
+    acceleration: 50
   });
 
 }).call(this);
