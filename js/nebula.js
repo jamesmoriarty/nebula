d963b718077674fe36e3f5b9dec2c2c2339489dc
James Moriarty <jamespaulmoriarty@gmail.com>
2013-11-05 16:30:43 +1100

    update: js.

--- js/nebula.js
+++ js/nebula.js
@@ -2,6 +2,143 @@
 (function() {
   var Q;
 
+  Q.component('aiHunter', {
+    added: function() {
+      return this.entity.on("step", this, "step");
+    },
+    step: function(dt) {
+      var targetAngle, targetDistance, _results;
+      if (!this.target) {
+        _results = [];
+        while (true) {
+          this.target = Q._shuffle(Q("SmallShip").items)[0];
+          if (this.target !== this.entity) {
+            break;
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      } else {
+        targetAngle = this.entity.p.angle - Q.angle(this.entity.p.x, this.entity.p.y, this.target.p.x, this.target.p.y);
+        if (targetAngle > 0) {
+          this.entity.turn(dt, -Q[this.entity.className].rotation);
+        } else {
+          this.entity.turn(dt, Q[this.entity.className].rotation);
+        }
+        targetDistance = Q.distance(this.entity.p.x, this.entity.p.y, this.target.p.x, this.target.p.y);
+        if (Math.abs(targetAngle) < 10 && targetDistance < 200) {
+          return this.entity.fire();
+        } else {
+          return this.entity.accelerate(dt);
+        }
+      }
+    }
+  });
+
+  Q.component('aiWander', {
+    added: function() {
+      return this.entity.on("step", this, "step");
+    },
+    step: function(dt) {
+      var targetAngle;
+      if (!this.target || 50 > Q.distance(this.entity.p.x, this.entity.p.y, this.target.p.x, this.target.p.y)) {
+        this.target = {
+          p: {
+            x: Math.random() * 1000,
+            y: Math.random() * 1000
+          }
+        };
+      }
+      targetAngle = this.entity.p.angle - Q.angle(this.entity.p.x, this.entity.p.y, this.target.p.x, this.target.p.y);
+      if (targetAngle > 0) {
+        this.entity.turn(dt, -Q[this.entity.className].rotation);
+      } else {
+        this.entity.turn(dt, Q[this.entity.className].rotation);
+      }
+      return this.entity.accelerate(dt);
+    }
+  });
+
+  Q.component('minimap', {
+    added: function() {
+      return this.entity.on("draw", this, "draw");
+    },
+    draw: function(ctx, width, height) {
+      var centerX, centerY, _this;
+      if (width == null) {
+        width = 100;
+      }
+      if (height == null) {
+        height = 100;
+      }
+      centerX = width / 2;
+      centerY = height / 2;
+      ctx.save();
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.translate(0, 0);
+      ctx.lineWidth = "2";
+      ctx.beginPath();
+      ctx.strokeStyle = "#FFF";
+      ctx.fillStyle = "rgba(255,255,255,0.1)";
+      ctx.rect(0, 0, width, height);
+      ctx.fill();
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.strokeStyle = "#00F";
+      ctx.rect(centerX, centerY, 1, 1);
+      ctx.stroke();
+      _this = this.entity;
+      ctx.beginPath();
+      Q("SmallShip").each(function() {
+        var x, y;
+        x = centerX - ((_this.p.x - this.p.x) / 100);
+        y = centerY - ((_this.p.y - this.p.y) / 100);
+        ctx.strokeStyle = "#F00";
+        ctx.rect(x, y, 1, 1);
+        return ctx.stroke();
+      });
+      return ctx.restore();
+    }
+  });
+
+  Q.component('player', {
+    added: function() {
+      return this.entity.on("step", this, "step");
+    },
+    step: function(dt) {
+      if (Q.inputs['up'] || Q.inputs['action']) {
+        this.entity.accelerate(dt);
+      } else {
+        this.entity.friction(dt);
+      }
+      if (Q.inputs['fire']) {
+        this.entity.fire();
+      }
+      if (Q.inputs['left']) {
+        this.entity.turn(dt, -Q[this.entity.className].rotation);
+      }
+      if (Q.inputs['right']) {
+        return this.entity.turn(dt, Q[this.entity.className].rotation);
+      }
+    }
+  });
+
+  Q.component('ttl', {
+    added: function() {
+      this.startedAt = this.now();
+      return this.entity.on("step", this, "step");
+    },
+    step: function() {
+      if (this.now() > this.startedAt + this.entity.p.ttl) {
+        return this.entity.destroy();
+      }
+    },
+    now: function() {
+      return new Date().getTime();
+    }
+  });
+
   Array.prototype.x = function() {
     return this[0];
   };
@@ -331,6 +468,21 @@
       this._super(ctx);
       return ctx.restore();
     }
+  });
+
+  Q.Ship.extend('SmallShip', {
+    init: function(p) {
+      this._super(Q._extend({
+        type: Q.SPRITE_DEFAULT | Q.SPRITE_ENEMY,
+        collisionMask: Q.SPRITE_ACTIVE,
+        asset: "ship" + (Math.floor((Math.random() * 5) + 1)) + ".png"
+      }, p));
+      return this.weapon = new Q.Blaster;
+    }
+  }, {
+    acceleration: 100,
+    rotation: 100,
+    maxVelocity: 200
   });
 
   Q.Sprite.extend('Star', {
